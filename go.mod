cfe85b7adf8ab3dda04715bb7e2ad2e30ed37d35
Rio <rkam1058@gmail.com>
2025-04-14 19:32:35 +1200

    hopium

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.23.6
 
 require github.com/slack-go/slack v0.16.0
 
+require github.com/dankeroni/jsonapi v0.0.0-20160914195301-7aa17e21f323 // indirect
+
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1
+	github.com/pajlada/gobttv v1.0.0
 )
--- src/main.go
+++ src/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/pajlada/gobttv"
 	"github.com/slack-go/slack"
 )
 
 const slashCommand = "/donowall"
 
-func slashCommandHandler() http.HandlerFunc {
+func slashCommandHandler(bttv *gobttv.BTTVAPI) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s, err := slack.SlashCommandParse(r)
 
@@ -31,11 +33,27 @@ func slashCommandHandler() http.HandlerFunc {
 			log.Printf("%s slack command is issued\n", slashCommand)
 			// Add code here!
 			response := "Hello World!"
+			params := &slack.Msg{Text: s.Text}
+			text := params.Text
+			emotes, err := bttv.GetEmotes()
+
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			emoteJson, err := json.Marshal(emotes)
+
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
 			// Create a JSON response
 			w.Header().Set("Content-Type", "application/json")
 			jsonResponse := make(map[string]string)
-			jsonResponse["response_type"] = "in_channel"
-			jsonResponse["text"] = response
+			jsonResponse["response_type"] = "ephemeral"
+			jsonResponse["text"] = fmt.Sprintf("%s : %s\n%s", response, text, emoteJson)
 			jsonResponseMarshal, err := json.Marshal(jsonResponse)
 			if err != nil {
 				log.Fatalf("error while JSON marshalling response: %s", err)
@@ -53,7 +71,9 @@ func slashCommandHandler() http.HandlerFunc {
 func main() {
 	godotenv.Load(".env")
 
-	http.HandleFunc("/receive", slashCommandHandler())
+	bttv := gobttv.New()
+
+	http.HandleFunc("/receive", slashCommandHandler(bttv))
 
 	port := os.Getenv("PORT")
 
