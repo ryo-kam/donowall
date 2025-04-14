bd25265c6092f77a4b614788e894c42b8c018e79
Ryo Kamata <ryo.kamata@wellnomics.com>
2025-04-15 07:55:29 +1200

    add replace_original to payload

--- src/client.go
+++ src/client.go
@@ -9,9 +9,10 @@ type Client struct {
 }
 
 type SlackResponse struct {
-	ResponseType string              `json:"response_type"`
-	Blocks       []map[string]string `json:"blocks"`
-	Text         string              `json:"text"`
+	ResponseType    string              `json:"response_type"`
+	ReplaceOriginal bool                `json:"replace_original"`
+	Text            string              `json:"text"`
+	Blocks          []map[string]string `json:"blocks"`
 }
 
 func NewClient() (*Client, error) {
@@ -30,8 +31,9 @@ func (client *Client) Get(code string) SlackResponse {
 
 	if err != nil {
 		return SlackResponse{
-			ResponseType: "in_channel",
-			Text:         fmt.Sprintf("error: %s", err.Error()),
+			ResponseType:    "in_channel",
+			ReplaceOriginal: true,
+			Text:            fmt.Sprintf("error: %s", err.Error()),
 		}
 	} else {
 		imgBlock := make(map[string]string)
@@ -41,9 +43,10 @@ func (client *Client) Get(code string) SlackResponse {
 		imgBlock["image_url"] = url
 
 		return SlackResponse{
-			ResponseType: "in_channel",
-			Text:         code,
-			Blocks:       []map[string]string{imgBlock},
+			ResponseType:    "in_channel",
+			ReplaceOriginal: true,
+			Text:            code,
+			Blocks:          []map[string]string{imgBlock},
 		}
 	}
 }
